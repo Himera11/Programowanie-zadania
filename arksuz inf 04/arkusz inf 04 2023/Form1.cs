bc5a99a467c7d1a9aad79850978dcc894e467234
Chimera11 <damian.manecki@gmail.com>
2024-04-04 21:20:25 +0200

    Update Form1.cs

--- arksuz inf 04/arkusz inf 04 2023/Form1.cs
+++ arksuz inf 04/arkusz inf 04 2023/Form1.cs
@@ -7,6 +7,39 @@ namespace arkusz_inf_04_2023
             InitializeComponent();
         }
 
+        Random random = new Random();
+        string duzelitery = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        string malelitery = "qwertyuiopasdfghjklzxcvbnm";
+        string znakispecjalne = "!@#$%^&*()-_=+";
+        string liczby = "0123456789";
+        string haslo = "";
+
+        
+        string RobHaslo(int dlugosc)
+        {
+            string _haslo = "";
+            while (_haslo. Length < dlugosc)
+            {
+                if (checkBox1.Checked)
+            {
+                _haslo + duze [los.Next(0, duze.Length)];
+            }        
+            if(_haslo.Length == dlugosc) return _haslo;
+            if (checkBox2.Checked)
+            {
+             _haslo += cyfry [los.Next(0, cyfry.Length)];
+            if (_haslo.Length == dlugosc) return _haslo;
+            }
+            if (checkBox3.Checked)
+            {
+                        _haslo += specjalne [los.Next(0, specjalne.Length)];
+
+            }
+        if (_haslo.Length == dlugosc) return _haslo;
+    }
+return _haslo;
+}
+        
         private void genhaslo_button_Click(object sender, EventArgs e)
         {
 
